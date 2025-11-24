80d9d9d7bfa68091be964a953e280ab70cc440d3
RokRage <jfy6d2zq29@privaterelay.appleid.com>
2025-11-24 12:58:20 +0000

    update simple json output

--- mirth_transform_HL7_to_JSON_ouput.js
+++ mirth_transform_HL7_to_JSON_ouput.js
@@ -1,17 +1,7 @@
-/**
- * Minimal HL7-to-JSON transformer for Mirth Connect.
- * Converts the parsed HL7 (E4X XML in msg) to JSON using the built-in serializer
- * and places the JSON string on the channel map.
- */
+// Under channel 'Set Data Types' set the outbound to 'JSON'
+// Under the source transformer change the outbound message template to JSON DataType
+// Add : "{}" as the outbound template content.
 
-// Serialize the HL7 XML tree to JSON string
-var jsonStr = SerializerFactory.getSerializer('JSON').fromXML(msg);
-
-// Make available to downstream steps/connectors
-channelMap.put('hl7Json', jsonStr);
-
-// Optionally log for debugging; comment out if noisy
-logger.info(jsonStr);
-
-// Return JSON if used in a JavaScript Transformer/Writer
-return jsonStr;
+// add this line to transform
+// ALL DONE!
+tmp = XmlUtil.toJson(msg)
